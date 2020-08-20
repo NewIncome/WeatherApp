0b4832abdb6d3b4c0024d17dd6829d04d7fb3ea6
NewIncome <alfredo_cardenas@live.com>
2020-08-19 19:13:26 -0500

    Add webpack-dev-server and test

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    // publicPath: '/dist/',
+  },
+  devServer: {
+    port: 1234,
+    contentBase: path.join(__dirname, 'dist'),
   },
 };

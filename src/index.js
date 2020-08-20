3dc465e66fdc2cdf3b14368027c40f8e4d48e6cf
NewIncome <alfredo_cardenas@live.com>
2020-08-20 17:06:50 -0500

    Fixed styles, add basic js for weather API data get

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import './styles/style.scss';
+
+const bttn = document.querySelector('.submit');
+const city = 'Naucalpan';
+
+async function getTemp() {
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=550a45bc4aa9613e4687926216e69765`);
+  const weatherData = await response.json();
+  console.log(weatherData);
+  console.log(weatherData.main);
+
+  console.log(weatherData.name);
+  console.log(weatherData.sys.country);
+
+  console.log(weatherData.main.feels_like);
+  console.log(weatherData.main.temp_max);
+  console.log(weatherData.main.temp_min);
+
+  console.log(weatherData.weather[0].main);
+  console.log(weatherData.weather[0].description);
+  console.log(weatherData.weather[0].icon);
+
+  console.log(weatherData.wind.speed);
+  console.log(weatherData.main.humidity);
+}
+
+bttn.onclick = getTemp();
